3a4a3d8dac7d5c7a0c41a00f40ad58605c7ce4b5
BOTAlex <zhentao2004@gmail.com>
2025-04-23 05:18:58 +0200

    Main layout done

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { onMount } from "svelte";
+    import "../style.css";
+    import Navbar from "./LayoutComps/Navbar.svelte";
+</script>
+
+<Navbar style="height: 15vh;"/>
+<div class="w-full grid drawer-end lg:drawer-open max-xl:h-[84vh] overflow-hidden">
+    <input id="SideDrawer" type="checkbox" class="drawer-toggle w-0" />
+    <div class="drawer-content flex flex-col items-center justify-center w-full h-full">
+        <!-- Page content here -->
+
+        <main class="w-full h-full">
+            <slot />
+        </main>
+
+    </div>
+</div>
+
---
+++ src/routes/LayoutComps/Navbar.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import GithubMark from "@images/brands/GithubMark.svelte";
+    import DeprivedDevsLogo from "@images/brands/DeprivedLogo.svg";
+
+    import { afterNavigate } from '$app/navigation';
+    import { onMount } from "svelte";
+    let currentPath = '/';
+
+    onMount(async ()=>{
+        currentPath = window.location.pathname;
+    });
+
+    afterNavigate(({ to }) => {
+        currentPath = to!.url.pathname;
+    });
+</script>
+
+<!-- svelte-ignore a11y_consider_explicit_label -->
+<!-- svelte-ignore a11y_missing_attribute -->
+<div {...$$restProps } class="bg-base-100">
+    <div class="flex flex-col w-full">
+        <div class="flex items-center p-2 shadow-lg">
+            <div class="w-10 h-10 mr-3">
+                <img class="object-contain" src="{DeprivedDevsLogo}" alt="">
+            </div>
+            <a class="p-0 text-xl">
+                <div class="font-bold">
+                    <div>Haptics extended</div>
+                </div>
+            </a>
+            <div class="ml-auto p-2">
+                <a href="https://github.com/MagicBOTAlex/TauriLearning" target="_blank">
+                    <div class="w-6 h-6object-contain fill-base-content">
+                        <GithubMark/>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div role="tablist" class="tabs tabs-border w-full shadow">
+            <a role="tab" class="tab {(currentPath == '/') ? 'tab-active' : ''}" href="/">Control</a>
+            <a role="tab" class="tab {(currentPath  == '/settings') ? 'tab-active' : ''}" href="/settings">Settings</a>
+        </div>
+    </div>
+</div>
