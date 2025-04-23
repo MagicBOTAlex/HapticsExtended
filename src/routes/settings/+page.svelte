a937ca2bdd59c50fa5a315b15bf24700bea8628d
BOTAlex <zhentao2004@gmail.com>
2025-04-23 10:42:25 +0200

    now able to parse OSC to typescript

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -2,24 +2,28 @@
     import { HapticSettings } from "@src/lib/stores";
     import InstanceRoute from "./SettingsComp/InstanceRoute.svelte";
     import { Plus } from "lucide-svelte";
-    import {invoke} from '@tauri-apps/api/core';
+    import { invoke } from "@tauri-apps/api/core";
 
-    invoke<string>(
-        "startOscServer"
-      );
+    invoke<string>("startOscServer");
 
-      import { listen } from '@tauri-apps/api/event';
+    import { listen } from "@tauri-apps/api/event";
+    import { OscPayload } from "@src/lib/types/OscPayload";
 
-    listen('rust-to-js', event => {
-        console.log('Received from Rust:', event.payload);
+    listen<OscPayload>("rust-to-js", (event) => {
+        let payload = OscPayload.fromRaw(event.payload);
+        console.log("Received from \"" + payload.address + "\"");
+        
+        for (let i = 0; i < payload.args.length; i++)  {
+            console.log(payload.args[i]);
+        }
+        console.log(payload);
     });
-
 </script>
 
 <div class="h-full bg-grid-100 w-full overflow-y-scroll relative">
     <div class="grid grid-cols-1 grid-flow-row p-4 gap-4 w-full">
         {#each HapticSettings.routes as route}
-        <InstanceRoute bind:route={route}/>
+            <InstanceRoute bind:route />
         {/each}
         <!-- {#each HapticSettings.routes as route}
         <InstanceRoute bind:route={route}/>
@@ -27,9 +31,8 @@
         {#each HapticSettings.routes as route}
         <InstanceRoute bind:route={route}/>
         {/each} -->
-
     </div>
     <div class="sticky z-10 bottom-0 right-0">
-        <button class="btn btn-success btn-square m-4"><Plus/></button>
+        <button class="btn btn-success btn-square m-4"><Plus /></button>
     </div>
 </div>
